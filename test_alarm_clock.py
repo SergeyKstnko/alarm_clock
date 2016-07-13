5bf9199b197b7b5451570d81c00577dc9fdc1c3c
Sergey <SergeyKstnko@users.noreply.github.com>
2016-07-13 18:53:27 -0400

    Update test_alarm_clock.py

--- test_alarm_clock.py
+++ test_alarm_clock.py
@@ -7,8 +7,9 @@ import unittest, sys
 import alarm_clock, time
 
 class TestStringMethods(unittest.TestCase):
+
 	def test_everything_ok(self):
-		'''Test_everything_ok Tests the case when the current time is equal to the alarm time.'''
+		'''Tests the case when the current time is equal to the alarm time.'''
 		if time.localtime().tm_hour > 12:
 			hr = time.localtime().tm_hour - 12
 			per = 'pm'
@@ -18,9 +19,9 @@ class TestStringMethods(unittest.TestCase):
 		alarm_clock.alarm(args['h'], args['m'], args['p'])
 
 	def test_command_line(self):
-		'''This tests command line arguments input'''
+		'''Tests command line arguments input'''
 		test_arguments = ['alarm_clock.py', '--h', '6', '--m', '05', '--p', 'pm']
-		alarm_clock.command_line(['--h', '12', '--m', '2', '--p', 'AM'])
+		alarm_clock.command_line(test_arguments[1:])
 
 if __name__ == '__main__':
 	unittest.main()
